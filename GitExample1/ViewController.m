7657142a788c21f3380167ef315febb55af285db
VgriOS5 <Vagaroios5@gmail.com>
2019-07-16 12:59:27 +0530

    - Reverse function added

--- GitExample1/ViewController.m
+++ GitExample1/ViewController.m
@@ -17,8 +17,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"viewDidLoad");
+    
+    NSLog(@"reverseString of  Jalpa : %@", [self reverseString:@"Jalpa"]);
     // Do any additional setup after loading the view.
 }
 
+-(NSMutableString*)reverseString:(NSString *)original{
+   
+    NSMutableString *reversedStr;
+    NSUInteger len = [original length];
+    
+    // Auto released string
+    reversedStr = [NSMutableString stringWithCapacity:len];
+    
+    // Probably woefully inefficient...
+    while (len > 0)
+        [reversedStr appendString:
+         [NSString stringWithFormat:@"%C", [original characterAtIndex:--len]]];
+    
+    return reversedStr;
+    
+}
 
 @end
